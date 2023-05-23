f1f14141a2796ecc60ab3d3e65e9dcca58a3c5c6
AwaisAmin <awaisamin090@gmail.com>
2023-05-23 10:02:07 +0500

    added imports

--- nft_smart_contract/contracts/NFT_Marketplace.sol
+++ nft_smart_contract/contracts/NFT_Marketplace.sol
@@ -1,2 +1,9 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
+
+// Internal Import For NFT OpenZeppelin 
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+import "hardhat/console.sol";
