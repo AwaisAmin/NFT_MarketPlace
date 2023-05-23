340d10d95f2f2acd75512c9856b6f0eadf02243f
AwaisAmin <awaisamin090@gmail.com>
2023-05-23 10:33:28 +0500

    writing contract and added listing method

--- nft_smart_contract/contracts/NFT_Marketplace.sol
+++ nft_smart_contract/contracts/NFT_Marketplace.sol
@@ -7,3 +7,48 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 import "hardhat/console.sol";
+
+contract NFTMarketplace is ERC721URIStorage {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIds;
+    Counters.Counter private _itemsSold;
+
+    uint256 listingPrice = 0.0015 ether;
+
+    address payable owner;
+
+    mapping(uint256 => MarketItem) private idMarketItem;
+
+    struct MarketItem {
+        uint256 tokenId;
+        address payable seller;
+        address payable owner;
+        uint256 price;
+        bool sold;
+    }
+
+    event idMartketItemCreated (
+        uint256 indexed tokenId,
+        address seller,
+        address owner,
+        uint256 price,
+        bool sold
+    );
+
+    modifier onlyOwner() {
+        require(
+            msg.sender == owner, 
+            "only owner of the marketplace can change the listing price"
+        );
+        _;
+    }
+
+    constructor() ERC721("NFT Metavarse Token", "MYNFT") {
+        owner == payable(msg.sender);
+    }
+
+    function updateListingPrice(uint256 _listingPrice) public payable onlyOwner {
+        listingPrice = _listingPrice;
+    }
+}
