a56f0b487fe74806819ba16ca7745525cb38a770
AwaisAmin <awaisamin090@gmail.com>
2023-05-23 12:29:21 +0500

    created purchased-item method

--- nft_smart_contract/contracts/NFT_Marketplace.sol
+++ nft_smart_contract/contracts/NFT_Marketplace.sol
@@ -156,4 +156,30 @@ contract NFTMarketplace is ERC721URIStorage {
 
         return items;
     }
+
+    // PURCHASE ITEM
+    function fetchMyNFT() public view returns(MarketItem[] memory) {
+        uint256 totalCount = _tokenIds.current();
+        uint256 itemCount = 0;
+        uint256 currentIndex = 0;
+
+        for(uint256 i = 0; i < totalCount; i++) {
+            if(idMarketItem[i + 1].owner == msg.sender) {
+                itemCount += 1;
+            }
+        }
+
+        MarketItem[] memory items = new MarketItem[](totalCount);
+
+        for(uint256 i = 0; i < totalCount; i++) {
+            if(idMarketItem[i + 1].owner == msg.sender) {
+                uint256 currentId = i + 1;
+                MarketItem storage currentItem = idMarketItem[currentId];
+                items[currentIndex] = currentItem;
+                currentIndex += 1;
+            }
+        }
+
+        return items;
+    }
 }
